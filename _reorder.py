1d96976dc844fa72e9cea6d7d53bfbbb1082256c
minorg <minorg@server.fake>
2014-02-14 13:12:40 -0600

    tweaking _reorder.py

--- _reorder.py
+++ _reorder.py
@@ -11,6 +11,8 @@ order = (
 )
 
 my_dir_path = os.path.dirname(__file__)
+if len(my_dir_path) == 0:
+    my_dir_path = os.getcwd()
 
 for file_name in os.listdir(my_dir_path):
     if os.path.splitext(file_name)[1] != '.py':
@@ -18,9 +20,16 @@ for file_name in os.listdir(my_dir_path):
     elif file_name[0] == '_' or file_name[0] == '.':
         continue
     file_path = os.path.join(my_dir_path, file_name)
-    new_file_path = os.path.join(my_dir_path, '_'.join(file_name.split('_', 2)[1:]))
+    file_name_split = file_name.split('_', 1)
+    if len(file_name_split) == 1:
+        continue
+    try:
+        file_number = int(file_name_split[0])
+    except ValueError:
+        continue
+    new_file_path = os.path.join(my_dir_path, file_name_split[1])
     print file_path, 'to', new_file_path
-    os.rename(file_path, new_file_path)
+     os.rename(file_path, new_file_path)
 
 for file_i, file_base_name in enumerate(order):
     file_path = os.path.join(my_dir_path, file_base_name + '.py')
