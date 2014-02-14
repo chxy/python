2ad616070c3fed800f5d364b762001ddda72cf01
minorg <minorg@server.fake>
2014-02-14 12:58:41 -0600

    task1.py

---
+++ task1.py
@@ -0,0 +1,3 @@
+# Create a tree of dicts from regents.csv
+# Print the contents of the tree with indentation for each level
+# Using the tree (and not the rows), count the number of graduate students and print the result
